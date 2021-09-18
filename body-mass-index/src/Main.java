964a1ccc08ba68b00b5e5049b2f210e9766dda5b
Viktoria <vikitoga@gmail.com>
2021-09-19 01:18:03 +0300

    Update project

--- body-mass-index/src/Main.java
+++ body-mass-index/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService bodyMassIndex = new BmiService();
-        double weight = 70.5;
-        double height = 1.80;
-        double bmi = bodyMassIndex.calculate(weight, height);
-        System.out.println("Индекс массы тела при весе - " + weight + " кг и росте - " + height + " м, составляет:");
+        double weightInKilograms = 70.5;
+        double heightInMeters = 1.80;
+        double bmi = bodyMassIndex.calculate(weightInKilograms, heightInMeters);
+        System.out.println("Индекс массы тела при весе - " + weightInKilograms + " кг и росте - " + heightInMeters + " м, составляет:");
         System.out.printf("%.2f", bmi);
     }
 }
